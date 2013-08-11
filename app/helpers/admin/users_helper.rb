55f57e21a7ac547b328b1cb253546a078f972dd0
Andrey Zamovskiy <nixoid@odesk.com>
2013-08-11 23:33:18 +0300

    upgraded to rails 3.2

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,9 +1,9 @@
 module Admin::UsersHelper
-  def id_column(record)
+  def id_column(record, column)
     record.id.to_s
   end
   
-  def name_column(record)
+  def name_column(record, column)
     link_to record.name, admin_user_account_operations_path(record)
   end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,10 @@ BitcoinBank::Application.configure do
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
 
+  # Raise exception on mass assignment protection for Active Record models
+  # uncomment this before test & commit
+  #config.active_record.mass_assignment_sanitizer = :strict
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
